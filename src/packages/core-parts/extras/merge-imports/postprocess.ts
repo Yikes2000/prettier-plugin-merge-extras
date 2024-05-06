1c6ea2907ca5d9a262c068b02d289254ecb6f3a6
Andy Lee <alee@intellisurvey.com>
2024-05-05 22:53:50 -0700

    Fix mergeSimpleImports

--- src/packages/core-parts/extras/merge-imports/postprocess.ts
+++ src/packages/core-parts/extras/merge-imports/postprocess.ts
@@ -1,4 +1,21 @@
-import { format } from 'prettier';
+/**
+ * @trivago/prettier-plugin-sort-imports doesn't combine imports of the same target:
+ *
+ *     import { a } from "foo";
+ *     import { b, c } from "foo";
+ *     import { d } from "foo";
+ *
+ * These import lines are adjacent, and sorted by first import element.  Function mergeSimpleImports() combines them into
+ * one line, then break into multi-line if necessary per printWidth option:
+ *
+ *     import { a, b, c, d } from "foo";
+ *     import {
+ *         Alpha,
+ *         Bravo,
+ *         Charlie,
+ *         Delta
+ *     } from "foo";
+ */
 
 interface MergeOptions {
   mergeSimpleImports?: boolean;
@@ -13,6 +30,8 @@ function setOptions(options: MergeOptions) {
 export function postprocess(code: string, options: any): string {
 
     setOptions(options);
+
+    // Skip if turned off, or empty code
     if (!mergeImports || code.match(/^\s*$/)) {
         return code;
     }
@@ -27,7 +46,7 @@ const RE_SPLIT_IMPORTS = new RegExp(/(?<=\nimport \{ [^\}]+ \} from [^\n]+\n)/);
 const RE_ADJ_IMPORTS = new RegExp(/(?<=\n)import \{ ([^\}]+) \} from (\S+);\nimport \{ ([^\}]+) \} from \2;\n/);
 
 /**
- * Merge adjacent simple imports.  (Temporary fix for trivago/prettier-plugin-sort-imports.)
+ * Merge adjacent simple imports.
  */
 function mergeSimpleImports(code: string, options: any): string {
 
@@ -36,20 +55,52 @@ function mergeSimpleImports(code: string, options: any): string {
     const codeBody = segs[segs.length - 1].match(/^import\s+/m) ? '' : segs.pop();
     let importLines = ''.concat(...segs);
 
-    // Merge adjacent imports
-    let mergeCount = 0;
+    // Combine adjacent import lines as appropriate
+    let changeCount = 0;
     while (importLines.match(RE_ADJ_IMPORTS)) {
         importLines = importLines.replace(RE_ADJ_IMPORTS, 'import { $1, $3 } from $2;\n');
-        mergeCount++;
+        changeCount++;
     }
-
-    if (mergeCount === 0) {
+    if (changeCount === 0) {
         return code;
     }
 
-    // Format multi-line imports
-    importLines = format(importLines, { ...options, plugins: [] });
+    // Break into individual lines and reformat into multi-line as needed
+    const newLines = importLines.split(/(?<=\n)/)
+        .map((line) => formatImport(line, options));
 
-    return importLines + codeBody;
+    return ''.concat(...newLines) + codeBody;
 }
-    
+
+// Detect import line with at least two import elements, e.g. "import { a, b } from ...;\n"
+const RE_IMPORT_LINE = new RegExp(/^import \{ ((?:[^,]+, )+[^,]+) \} from ([^\n]+);\n$/);
+
+/**
+ * Sort import elements and break into multi-lie as needed
+ */
+function formatImport(origLine: string, options: any): string {
+    let newLines;
+
+    // Skip unless an import line with at least two elements
+    if (!RE_IMPORT_LINE.test(origLine)) {
+        return origLine;
+    }
+
+    const match = origLine.match(RE_IMPORT_LINE);
+    const elements = match[1].split(/, /).sort();  // sorted elements
+    const target = match[2];
+
+    // Reconstruct import line, or multi-line if necessary
+    if (origLine.length > options.printWidth) {
+
+        newLines = 'import {\n'
+            + elements.map((x) => ' '.repeat(options.tabWidth) + x).join(',\n')
+            + ( options.tlainingCommas === 'none' ? '' : ',' ) + '\n'
+            + `} from ${target};\n`;
+    }
+    else {
+        newLines = 'import { ' + elements.join(', ') + ` } from ${target};\n`;
+    }
+
+    return newLines;
+}
