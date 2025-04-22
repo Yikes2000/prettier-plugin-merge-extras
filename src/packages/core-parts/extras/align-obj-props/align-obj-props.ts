258b62e4d7b649a1eea0e6daf081313c64f23849
Andy Lee <alee@intellisurvey.com>
2025-04-22 00:02:35 -0700

    Angular .html template '(document:event)=...'

--- src/packages/core-parts/extras/align-obj-props/align-obj-props.ts
+++ src/packages/core-parts/extras/align-obj-props/align-obj-props.ts
@@ -280,7 +280,7 @@ function alignObjectPropLevel(segs: any[], idx: number[], indent: number) {
  * RegExp for property key 'key:', '[key]:', '"key":', "'key':", '#key:', 'key$:', etc
  */
 function getRegExpProperty(indent: number) {
-    return new RegExp(`^[ ]{${indent}}((?:[^'" :]+|(['"])[^\\2\\n]+?\\2|\\[[^\\]]+\\])\\??):`);
+    return new RegExp(`^[ ]{${indent}}((?:[^'" :(]+|(['"])[^\\2\\n]+?\\2|\\[[^\\]]+\\])\\??):`);
 }
 
 /**
